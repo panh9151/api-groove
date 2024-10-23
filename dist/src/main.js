59622b3d51833a1ddc20abc6fe5ea76e3933e9d0
panh9151 <panh9151@gmail.com>
2024-10-23 13:00:39 +0700

    fix cors 6

--- dist/src/main.js
+++ dist/src/main.js
@@ -4,10 +4,16 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, {
+        cors: {
+            origin: true,
+            methods: "GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS",
+            allowedHeaders: ["Content-Type", "Authorization", "token"],
+            credentials: true,
+        },
+    });
     app.useGlobalPipes(new common_1.ValidationPipe());
-    app.enableCors();
-    await app.listen(3000);
+    await app.listen(4000);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,16 @@ import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: true,
+      methods: "GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS",
+      allowedHeaders: ["Content-Type", "Authorization", "token"], // Thêm các header mà bạn đang sử dụng
+      credentials: true,
+    },
+  });
+  // app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
