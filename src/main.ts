9f497d77f16d127b21d78bff68d99e3c8708993b
panh9151 <panh9151@gmail.com>
2024-10-23 10:00:04 +0700

    fix cors 4

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   // app.enableCors();
-  // app.enableCors({
-  //   origin: "*", // Cho phép mọi nguồn truy cập
-  //   methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS", "HEAD"], // Các phương thức được phép
-  //   allowedHeaders: ["Content-Type", "Authorization", "token"], // Cho phép mọi header
-  //   credentials: true, // Nếu cần thiết
-  // });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
