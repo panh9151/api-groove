523671d693cf6e490605fea775193807b971e111
panh9151 <panh9151@gmail.com>
2024-10-29 15:57:03 +0700

    A

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,20 @@ async function bootstrap() {
     ],
   });
 
+  SwaggerModule.setup("/", app, document, {
+    customSiteTitle: "Groove - API Documentaion",
+    customfavIcon: "https://avatars.githubusercontent.com/u/6936373?s=200&v=4",
+    customJs: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js",
+    ],
+    customCssUrl: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css",
+    ],
+  });
+
   // app.enableCors();
   app.enableCors();
   await app.listen(4000);
