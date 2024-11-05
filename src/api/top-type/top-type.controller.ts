ae890814adf4edea0c49e13860b49eeea8001d0d
panh9151 <panh9151@gmail.com>
2024-11-06 00:05:18 +0700

    a

--- src/api/top-type/top-type.controller.ts
+++ src/api/top-type/top-type.controller.ts
@@ -1,17 +1,5 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  Query,
-} from "@nestjs/common";
+import { Controller, Get, Query } from "@nestjs/common";
 import { TopTypeService } from "./top-type.service";
-import { CreateTopTypeDto } from "./dto/create-top-type.dto";
-import { UpdateTopTypeDto } from "./dto/update-top-type.dto";
-
 @Controller("top-type")
 export class TopTypeController {
   constructor(private readonly topTypeService: TopTypeService) {}
