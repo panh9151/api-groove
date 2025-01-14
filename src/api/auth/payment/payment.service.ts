0148cea1b2e586cd198dd19266f749a64ef0a11b
panh9151 <panh9151@gmail.com>
2025-01-15 05:43:34 +0700

    a

--- src/api/auth/payment/payment.service.ts
+++ src/api/auth/payment/payment.service.ts
@@ -5,6 +5,7 @@ import { Repository } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
 import { UpdatePaymentDto } from "./dto/update-payment.dto";
 import { UserEntity } from "../register/entities/register.entity";
+import { MailerService } from "@nestjs-modules/mailer";
 
 @Injectable()
 export class PaymentService {
@@ -12,7 +13,8 @@ export class PaymentService {
     @InjectRepository(PaymentEntity)
     private readonly paymentRepo: Repository<PaymentEntity>,
     @InjectRepository(UserEntity)
-    private readonly userRepo: Repository<UserEntity>
+    private readonly userRepo: Repository<UserEntity>,
+    private readonly mailerService: MailerService
   ) {}
 
   async create(body: CreatePaymentDto, req: any) {
@@ -43,9 +45,37 @@ export class PaymentService {
       const user = await this.userRepo.findOne({
         where: { id_user: req?.user?.id_user },
       });
+
       if (!user) {
         throw new NotFoundException("User not found");
       }
+
+      const paymentTime = new Date().toLocaleString("vi-VN", {
+        timeZone: "Asia/Ho_Chi_Minh",
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      });
+
+      await this.mailerService.sendMail({
+        to: req?.user?.email,
+        subject: "Groove - Xác nhận thanh toán gói VIP thành công",
+        text: `Kính gửi [Tên Người Dùng],
+        Chúc mừng bạn đã nâng cấp thành công gói VIP của Groove!
+        Chi tiết thanh toán:
+        - Gói thanh toán: 200.000 VND
+        - Thời gian thanh toán: ${paymentTime}
+        - Mã giao dịch: ${existingPayment?.id_payment}
+        Với gói VIP, bạn sẽ được tận hưởng nhiều tính năng cao cấp và độc quyền, giúp trải nghiệm âm nhạc của bạn thêm phong phú.
+        Nếu bạn có bất kỳ thắc mắc nào hoặc cần hỗ trợ, vui lòng liên hệ với đội ngũ hỗ trợ của chúng tôi qua zalo: (+84)794 437 748.
+        Cảm ơn bạn đã tin tưởng và đồng hành cùng Groove!
+        Trân trọng,
+        Đội ngũ Groove`,
+      });
 
       user.is_vip = 1; // Trực tiếp gán thay vì Object.assign
       await this.userRepo.save(user);
