ccf26d529bc9a932bb4cb10cc2243f0d71e21bb2
panh9151 <panh9151@gmail.com>
2025-01-15 05:52:20 +0700

    a

--- src/api/auth/payment/payment.service.ts
+++ src/api/auth/payment/payment.service.ts
@@ -64,17 +64,23 @@ export class PaymentService {
       await this.mailerService.sendMail({
         to: req?.user?.email,
         subject: "Groove - Xác nhận thanh toán gói VIP thành công",
-        text: `Kính gửi [Tên Người Dùng],
-        Chúc mừng bạn đã nâng cấp thành công gói VIP của Groove!
-        Chi tiết thanh toán:
-        - Gói thanh toán: 200.000 VND
-        - Thời gian thanh toán: ${paymentTime}
-        - Mã giao dịch: ${existingPayment?.id_payment}
-        Với gói VIP, bạn sẽ được tận hưởng nhiều tính năng cao cấp và độc quyền, giúp trải nghiệm âm nhạc của bạn thêm phong phú.
-        Nếu bạn có bất kỳ thắc mắc nào hoặc cần hỗ trợ, vui lòng liên hệ với đội ngũ hỗ trợ của chúng tôi qua zalo: (+84)794 437 748.
-        Cảm ơn bạn đã tin tưởng và đồng hành cùng Groove!
-        Trân trọng,
-        Đội ngũ Groove`,
+        text: `Kính gửi ${req?.user?.fullname},
+        
+Chúc mừng bạn đã nâng cấp thành công gói VIP của Groove!
+
+Chi tiết thanh toán:
+- Gói thanh toán: 200.000 VND
+- Thời gian thanh toán: ${paymentTime}
+- Mã giao dịch: ${existingPayment?.id_payment}
+
+Với gói VIP, bạn sẽ được tận hưởng nhiều tính năng cao cấp và độc quyền, giúp trải nghiệm âm nhạc của bạn thêm phong phú.
+
+Nếu bạn có bất kỳ thắc mắc nào hoặc cần hỗ trợ, vui lòng liên hệ với đội ngũ hỗ trợ của chúng tôi qua zalo: (+84)794 437 748.
+
+Cảm ơn bạn đã tin tưởng và đồng hành cùng Groove!
+
+Trân trọng,
+Đội ngũ Groove`,
       });
 
       user.is_vip = 1; // Trực tiếp gán thay vì Object.assign
